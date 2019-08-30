77ff0ff77ea022eb20851c48fe2784204ffa4242
Dev-01 <development@evonova.mx>
2019-08-30 18:01:44 -0500

    Changes in design

--- Week-1/tensorflow-classifier/canvas-controller.js
+++ Week-1/tensorflow-classifier/canvas-controller.js
@@ -1,22 +1,22 @@
 document.body.addEventListener('click', (e) => {
     if(e.target.id == 'nav-1'){
         //Change in tabs
-        document.getElementById('nav-2').classList.remove('active')
-        document.getElementById('nav-1').classList.add('active')
-
+        document.getElementById('nav-2').classList.remove('active');
+        document.getElementById('nav-1').classList.add('active');
+        
+        
         //Display block
-        document.getElementById('classifier').style.display ="block"
-        document.getElementById('real-time-detection').style.display ="none"
+        document.getElementById('real-time-detection').style.display ="block";
+        document.getElementById('classifier').style.display ="none";
 
     }else if(e.target.id == 'nav-2'){
         //Change in tabs
-        document.getElementById('nav-1').classList.remove('active')
-        document.getElementById('nav-2').classList.add('active')
+        document.getElementById('nav-1').classList.remove('active');
+        document.getElementById('nav-2').classList.add('active');
 
         //Display block
-
-        document.getElementById('classifier').style.display ="none"
-        document.getElementById('real-time-detection').style.display ="block"
+        document.getElementById('classifier').style.display ="block";
+        document.getElementById('real-time-detection').style.display ="none";
     }
 });
 
