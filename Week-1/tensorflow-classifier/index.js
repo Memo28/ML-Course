2ef689eefcc66fc281339066bf79056f1df3cadd
Memo <memovdg@gmail.com>
2019-08-30 22:01:07 -0500

    Controlling the canvas drawing with handsignal

--- Week-1/tensorflow-classifier/index.js
+++ Week-1/tensorflow-classifier/index.js
@@ -3,62 +3,75 @@ const classifier = knnClassifier.create();
 let net;
 const webcamElement = document.getElementById('webcam');
 
-async function realtime(){
+
+//Canvas load
+var canv = document.getElementById("canvas_space");
+var paper = canv.getContext("2d");
+var x = 200;
+var y = 200;
+draw("red", x - 1, y - 1, x + 1, y - 1, paper);
+
+
+async function realtime() {
 	await load();
 	//Cargamos la camara
 	await setupWebcam();
 
 
 	//Leemos las imagenes cargadas
-	const addExample = classId =>{
+	const addExample = classId => {
 		const activation = net.infer(webcamElement, 'conv_preds');
 
 		classifier.addExample(activation, classId);
 	};
 
-  	document.getElementById('class-a').addEventListener('click', () => addExample(0));
-	document.getElementById('class-b').addEventListener('click', () => addExample(1));
-	document.getElementById('class-c').addEventListener('click', () => addExample(2));
+	document.getElementById('move-up').addEventListener('click', () => addExample(0));
+	document.getElementById('move-down').addEventListener('click', () => addExample(1));
+	document.getElementById('move-left').addEventListener('click', () => addExample(2));
+	document.getElementById('move-right').addEventListener('click', () => addExample(2));
 
 
-	while(true){
+	while (true) {
 
-		if(classifier.getNumClasses () > 0){
+		if (classifier.getNumClasses() > 0) {
 			const activation = net.infer(webcamElement, 'conv_preds');
+
 
 			const result = await classifier.predictClass(activation);
 
-			const classes = ['A', 'B', 'C'];
-			document.getElementById('console').innerText = `prediction : ${classes[result.classIndex]}\n  probability : ${result.confidences[result.classIndex]}`;
-		}
+			const classes = ['UP', 'DOWN', 'LEFT', 'RIGHT'];
+			document.getElementById('display').innerText = `Move To : prediction : ${classes[result.classIndex]}\n  probability : ${result.confidences[result.classIndex]}`;
 
+			await moveInCanvas(classes[result.classIndex]);
+
+		}
 		await tf.nextFrame();
 	}
 
 }
 
 
-async function setupWebcam(){
-	return new Promise((resolve, reject) =>{
+async function setupWebcam() {
+	return new Promise((resolve, reject) => {
 		const navigatorAny = navigator;
 
 		navigator.getUserMedia = navigator.getUserMedia || navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia || navigatorAny.msGetUserMedia;
 
-		if(navigator.getUserMedia){
-			navigator.getUserMedia({video : true},
-				stream =>{
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({ video: true },
+				stream => {
 					webcamElement.srcObject = stream;
 					webcamElement.addEventListener('loadeddata', () => resolve(), false);
 				},
 				error => reject());
-		}else{
+		} else {
 			reject();
 		}
 	});
 }
 
 
-async function load(){
+async function load() {
 	console.log('Loading mobilenet...');
 
 	//Load the model
@@ -77,7 +90,7 @@ async function classifierFunction() {
 
 
 
-	result.forEach((item) =>{
+	result.forEach((item) => {
 		let li = document.createElement('li');
 		ul.appendChild(li);
 		li.innerHTML += "Class Name: " + item.className + " Probability : " + item.probability;
@@ -85,7 +98,54 @@ async function classifierFunction() {
 
 	console.log(result);
 
-	
+
+}
+
+
+async function moveInCanvas(position) {
+	var movement = 1;
+	switch (position) {
+		case 'UP':
+			//A little pause
+			draw("red", x, y, x, y - movement, paper);
+			y = y - movement;
+			setTimeout(() => { }, 10000);
+			console.log('UP');
+			break;
+		case 'DOWN':
+			draw("red", x, y, x, y + movement, paper);
+			y = y + movement;
+			setTimeout(() => { }, 10000);
+			console.log('DOWN');
+			break;
+		case 'LEFT':
+			draw("red", x, y, x - movement, y, paper);
+			x = x - movement;
+			setTimeout(() => { }, 10000);
+			console.log('LEFT');
+			break;
+		case 'RIGHT':
+			draw("red", x, y, x + movement, y, paper);
+			x = x + movement;
+			setTimeout(() => { }, 10000);
+			console.log('RIGHT');
+			break;
+		default:
+			setTimeout(() => { }, 10000);
+			break;
+	}
+}
+
+
+
+function draw(color, xstart, ystart, xfinal, yfinal, canv) {
+	canv.beginPath();
+	canv.strokeStyle = color;
+	canv.lineWidth = 3;
+	canv.moveTo(xstart, ystart);
+	canv.lineTo(xfinal, yfinal);
+	canv.stroke();
+	canv.closePath();
 }
 
 // app()
